7bd42a1bbc1d7c075656e6861404c7b6eba5f4b6
liam-kelley <liam.kelley.144@gmail.com>
2022-10-16 14:41:47 +0200

    Implementing select computing into eval.py

--- run_multiple_evals.py
+++ run_multiple_evals.py
@@ -2,20 +2,25 @@
 
 import os
 
-tags = [
+models_to_evaluate = [
         'unsupervised_2s_satb_bcbq_mf0_1',
         'unsupervised_4s_satb_bcbq_mf0_1_again'
         ]
 
 # eval_mode='default' # default evaluation
-eval_mode='fast' # default evaluation
+eval_mode='fast' # fast evaluation
 
-if eval_mode=='default':
-    for tag in tags:
+if eval_mode=='original':
+    for tag in models_to_evaluate:
+        print("python eval.py --tag '{}' --f0-from-mix --test-set 'CSD'".format(tag))
+        os.system("python eval.py --tag '{}' --f0-from-mix --test-set 'CSD'".format(tag))
+
+elif eval_mode=='default':
+    for tag in models_to_evaluate:
         print("python eval.py --tag '{}' --f0-from-mix --test-set 'CSD' --show-progress --compute all".format(tag))
         os.system("python eval.py --tag '{}' --f0-from-mix --test-set 'CSD' --show-progress --compute all".format(tag))
 
 elif eval_mode=='fast':
-    for tag in tags:
+    for tag in models_to_evaluate:
         print("python eval.py --tag '{}' --f0-from-mix --test-set 'CSD' --show-progress --compute SI-SDR_mask".format(tag))
         os.system("python eval.py --tag '{}' --f0-from-mix --test-set 'CSD' --show-progress --compute SI-SDR_mask".format(tag))
